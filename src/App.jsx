f5ed502880712e9052bb93e3e4e7c0ce8c456aea
LoicVinci <loic.hernaut@student.vinci.be>
2022-11-15 17:52:59 +0100

    Add Paypal Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js"
 
 import CheckoutForm from "./CheckoutForm";
 import "./App.css";
@@ -35,12 +36,39 @@ export default function App() {
   };
 
   return (
-    <div className="App">
-      {clientSecret && (
-        <Elements options={options} stripe={stripePromise}>
-          <CheckoutForm />
-        </Elements>
-      )}
+    <div>
+      {/*<div className="App">
+        {clientSecret && (
+          <Elements options={options} stripe={stripePromise}>
+            <CheckoutForm />
+          </Elements>
+        )}
+        </div>*/}
+      <div>
+          <p>Danette Chocolat = 4$</p>
+          <PayPalScriptProvider options={{"client-id": "AXcTRDeJ9rEJ1qB7kyZStKJTi9sWpxI474fG2oOK84PTBXMvh0N19sBzTxOTMUaxK9qvE3Ukks6VbOdk"}}>
+            <PayPalButtons 
+              createOrder={(data, actions) => {
+                return actions.order.create({
+                        purchase_units: [
+                            {
+                                amount: {
+                                    value: "45.00",
+                                },
+                            },
+                        ],
+                    });
+              }}
+              onApprove={(data, actions) => {
+                return actions.order.capture().then(function(details) {
+                  // This function shows a transaction success message to your buyer.
+                  alert('Transaction completed by ' + details.payer.name.given_name);
+                });
+              }}
+            />
+        </PayPalScriptProvider>
+      </div>
     </div>
   );
 }
+//Ajouter Client ID dans .env voir vidéo PayPalDeveloper
