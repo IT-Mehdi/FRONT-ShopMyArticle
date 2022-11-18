9bda4bbda39742695d1f85fb18d86068ab173644
LoicVinci <loic.hernaut@student.vinci.be>
2022-11-18 12:26:28 +0100

    Fix Paypal Branch

--- src/App.jsx
+++ src/App.jsx
@@ -11,39 +11,11 @@ import "./App.css";
 // This is a public sample test API key.
 // Don’t submit any personally identifiable information in requests made with this key.
 // Sign in to see your own test API key embedded in code samples.
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
 
 export default function App() {
-  const [clientSecret, setClientSecret] = useState("");
-
-  useEffect(() => {
-    // Create PaymentIntent as soon as the page loads
-    fetch("/create-payment-intent", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
-    })
-      .then((res) => res.json())
-      .then((data) => setClientSecret(data.clientSecret));
-  }, []);
-
-  const appearance = {
-    theme: 'stripe',
-  };
-  const options = {
-    clientSecret,
-    appearance,
-  };
 
   return (
     <div>
-      {/*<div className="App">
-        {clientSecret && (
-          <Elements options={options} stripe={stripePromise}>
-            <CheckoutForm />
-          </Elements>
-        )}
-        </div>*/}
       <div>
           <p>Danette Chocolat = 4$</p>
           <PayPalScriptProvider options={{"client-id": "AXcTRDeJ9rEJ1qB7kyZStKJTi9sWpxI474fG2oOK84PTBXMvh0N19sBzTxOTMUaxK9qvE3Ukks6VbOdk"}}>
